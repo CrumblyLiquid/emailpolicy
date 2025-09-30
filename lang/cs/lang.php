f1689f58c4de7b66db92fa707a8423a5c9c221ac
CrumblyLiquid <crumblyliquid@gmail.com>
2025-09-30 21:54:58 +0000

    Create lang.php

---
+++ lang/cs/lang.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * czech language file for emailpolicy plugin
+ *
+ * @author CrumblyLiquid <crumblyliquid@gmail.com>
+ */
+ 
+$lang['allow_failed'] = "Email musí být mezi těmito doménami: ";
+$lang['deny_failed'] = "Email nesmí být mezi těmito doménami: ";
