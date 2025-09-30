d31faed125b3548bef3170e4e45a2746f7441aa2
CrumblyLiquid <crumblyliquid@gmail.com>
2025-09-30 21:58:16 +0000

    Create settings.php

---
+++ lang/cs/settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * czech language file for emailpolicy plugin
+ *
+ * @author CrumblyLiquid <crumblyliquid@gmail.com>
+ */
+
+$lang['allow'] = 'Seznam povolených domén oddělených čárkami. Pokud toto pole není prázdné, jen maily odpovídající specifikovaným doménám budou akceptovány.';
+$lang['deny'] = 'Seznam zakázaných domén oddělených čárkami. Emaily jsou testovány nejdříve proti whitelistu.';
