c616c2404c87546d96fd423a846c7624921a765d
CrumblyLiquid <crumblyliquid@gmail.com>
2025-09-30 21:45:00 +0000

    Update action.php

--- action.php
+++ action.php
@@ -51,11 +51,11 @@ class action_plugin_emailpolicy extends ActionPlugin
         } else {
             return;
         }
-        $allow = $this->getConf('allow');
-        $deny = $this->getConf('deny');
-        if (count((array)$allow) > 0)  {
+        $allow = (array)$this->getConf('allow');
+        $deny = (array)$this->getConf('deny');
+        if (count($allow) > 0)  {
             $pass = false;
-            foreach ((array)$allow as $x) {
+            foreach ($allow as $x) {
                 if ($this->endsWith($email, $x)) {
                      $pass = true;
                 }
@@ -66,8 +66,8 @@ class action_plugin_emailpolicy extends ActionPlugin
                 $event->stopPropagation();
             }
         }
-        if (count((array)$deny) > 0) {
-            foreach ((array)$deny as $x) {
+        if (count($deny) > 0) {
+            foreach ($deny as $x) {
                 if ($this->endsWith($email, $x)) {
                     msg($this->getLang('deny_failed') . implode(', ',$deny), -1);
                     $event->preventDefault();
